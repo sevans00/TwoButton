dff9080c7debc4c0b6509e5858807a6c799caed4
Shaun Evans <shaunevans.git@gmail.com>
2014-02-23 22:17:22 -0800

    ToggleBlocks!

---
+++ TwoButton/Assets/Scripts/TileMap/ToggleBlock.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//A one-way block
+public class ToggleBlock : InteractiveTile {
+
+	public bool On = true;
+	private bool starting_On;
+
+	private string spriteNamePrefix = "toggleblock_blue_";
+	private tk2dSprite sprite;
+	private BoxCollider2D boxcollider;
+
+	public void Start () {
+		sprite = GetComponent<tk2dSprite>();
+		//Box collider setup:
+		boxcollider = GetComponent<BoxCollider2D>();
+		starting_On = On;
+		On = !On;
+		Toggle(); //Setup toggle ;)
+	}
+	
+	public void Toggle () {
+		On = !On;
+		Color c = new Color(1f,1f,1f);
+		if ( On ) {
+			sprite.SetSprite( spriteNamePrefix + "on" );
+			boxcollider.enabled = true;
+			c.a = 1.0f;
+
+		} else {
+			sprite.SetSprite( spriteNamePrefix + "off" );
+			boxcollider.enabled = false;
+			c.a = 0.6f;
+		}
+		sprite.color = c;
+	}
+
+	override public void Reset () {
+		On = !starting_On;
+		Toggle();
+		base.Reset();
+	}
+
+
+}
---
+++ TwoButton/Assets/Scripts/TileMap/ToggleSwitch.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//A one-way block
+public class ToggleSwitch : InteractiveTile {
+
+
+	public bool Pressed = false;
+	private bool starting_Pressed;
+
+	private string spriteNamePrefix = "switch_";
+	private tk2dSprite sprite;
+	private ToggleBlock[] blocks;
+
+	public void Start () {
+		starting_Pressed = Pressed;
+		sprite = GetComponent<tk2dSprite>();
+		blocks = GameObject.FindObjectsOfType<ToggleBlock>();
+	}
+	
+	public void Toggle () {
+		Pressed = !Pressed;
+		if ( !Pressed ) {
+			sprite.SetSprite( spriteNamePrefix + "on" );
+		} else {
+			sprite.SetSprite( spriteNamePrefix + "off" );
+		}
+		foreach ( ToggleBlock block in blocks ) {
+			block.Toggle();
+		}
+	}
+
+	void OnSpriteTriggerEnter () {
+		Toggle();
+	}
+
+	
+	override public void Reset () {
+		Pressed = starting_Pressed;
+		if ( !Pressed ) {
+			sprite.SetSprite( spriteNamePrefix + "on" );
+		} else {
+			sprite.SetSprite( spriteNamePrefix + "off" );
+		}
+		base.Reset();
+	}
+
+}
